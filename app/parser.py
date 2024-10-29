74c58f8e5dadc2dcb57a759993d53c265778d5cf
GlebMozglyakov <mozglgleb2004@gmail.com>
2024-10-29 13:36:30 +0500

    feat: update

---
+++ app/parser.py
@@ -0,0 +1,107 @@
+from tkinter.font import names
+
+import requests
+from bs4 import BeautifulSoup
+from schemas import ProductBase
+import json
+
+
+def fetch_page(url):
+    """
+    Функция получения страницы
+    """
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"
+    }
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.text
+    else:
+        print(f"Ошибка при загрузке страницы: {response.status_code}")
+        return None
+
+
+def parse_products_from_cur_page(html):
+    """
+    Парсим продукты со страницы
+    """
+    soup = BeautifulSoup(html, 'html.parser')
+    products = []
+    pydantic_products = []
+
+    # Задаем селектор для карточек товаров
+    product_cards = soup.select(".ProductCard_product__2sBWu")
+
+    for card in product_cards:
+        # Извлекаем название и бренд
+        brand = card.select_one(".ProductCard_product__brand__WozgB").get_text(strip=True) if card.select_one(
+            ".ProductCard_product__brand__WozgB") else "Бренд не найден"
+        title = card.select_one(".heading_heading_5__sKtfG").get_text(strip=True) if card.select_one(
+            ".heading_heading_5__sKtfG") else "Название не найдено"
+
+        # Извлекаем цену
+        price = card.select_one(".ProductCard_product__price__8Yr1C").get_text(strip=True) if card.select_one(
+            ".ProductCard_product__price__8Yr1C") else "Цена не указана"
+
+        price = int(price.replace('₽', '').replace(' ', '').strip())
+
+        # Добавляем данные в список
+        products.append({
+            "brand": brand,
+            "name": title,
+            "price": price
+        })
+
+        pydantic_products.append(
+            ProductBase(brand=brand, name=title, price=price)
+        )
+
+
+    return products, pydantic_products
+
+
+def get_all_products():
+    """
+    Проходимся по всем страницам в новинках
+    """
+    base_url = "https://pkrovsky.com/men/new/"
+    page_number = 1
+    all_products = []
+    all_pydantic_products = []
+
+    while True:
+        url = f"{base_url}?page={page_number}"
+        print(f"Сбор данных со страницы: {url}")
+
+        html = fetch_page(url)
+        if html is None:
+            break
+
+        products, pydantic_products = parse_products_from_cur_page(html)
+        if not products:
+            print("Товары не найдены, завершение парсинга.")
+            break
+
+        all_products.extend(products)
+        all_pydantic_products.extend(pydantic_products)
+        page_number += 1
+
+    # Записываем товары в json
+    with open('data/products.json', 'w', encoding='utf-8') as file:
+        json.dump(all_products, file, ensure_ascii=False, indent=4)
+
+    return all_products, all_pydantic_products
+
+
+# Запуск парсера
+products, pydantic_products = get_all_products()
+
+# Вывод собранных данных
+# for product in products:
+#     print(f"Название: {product['name']}, Бренд: {product['brand']}, Цена: {product['price']}")
+
+print(f"len {len(pydantic_products)}")
+
+for product in pydantic_products:
+    print(product)
